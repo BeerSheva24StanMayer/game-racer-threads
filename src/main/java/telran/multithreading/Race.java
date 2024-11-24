b01f71f1d6968d89de65ab880e3a0f90cd5c1d6d
Stan Mayer <sthumor@gmail.com>
2024-11-24 19:32:24 +0200

    HW43_threads_mutex

--- src/main/java/telran/multithreading/Race.java
+++ src/main/java/telran/multithreading/Race.java
@@ -1,22 +1,30 @@
 
 package telran.multithreading;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Race {
 	private int distance;
 	private int minSleep;
 	private int maxSleep;
-	AtomicInteger winner = new AtomicInteger(-1);
+    private List<Integer[]> finishOrder = new ArrayList<>();
+    private LocalTime raceStartTime;
+
+
 	public Race(int distance, int minSleep, int maxSleep) {
 		this.distance = distance;
 		this.minSleep = minSleep;
 		this.maxSleep = maxSleep;
 	}
-	public int getWinner() {
-		return winner.get();
-	}
 	
+    public static LocalTime getStartTime(LocalTime raceStartTime) {
+        raceStartTime = LocalTime.now();
+        return raceStartTime;
+    }
+
 	public int getDistance() {
 		return distance;
 	}
@@ -27,4 +35,17 @@ public class Race {
 		return maxSleep;
 	}
 	
+    public void setRaceStart (LocalTime raceStartTime) {
+        this.raceStartTime = raceStartTime;
+    }
+
+    public void addRacer (int number) {
+        long raceTime = Duration.between(raceStartTime, LocalTime.now()).toMillis();
+        Integer[] racer = {number, (int) raceTime};
+        finishOrder.add(racer);
+    }
+
+    public List<Integer[]> getFinishOrder () {
+        return new ArrayList<>(finishOrder);
+    }
 }
--- src/main/java/telran/multithreading/Racer.java
+++ src/main/java/telran/multithreading/Racer.java
@@ -5,22 +5,26 @@ import java.util.Random;
 public class Racer extends Thread {
 private Race race;
 private int number;
+
 public Racer(Race race, int number) {
-	this.race = race;
-	this.number = number;
+    this.race = race;
+    this.number = number;  
 }
 @Override
 public void run() {
-	int minSleep = race.getMinSleep();
-	int maxSleep = race.getMaxSleep();
-	int distance = race.getDistance();
-	Random random = new Random();
-	for (int i = 0; i < distance; i++) {
-		try {
-			sleep(random.nextInt(minSleep, maxSleep + 1));
-			System.out.printf("%d - step %d\n",number, i);
-		} catch (InterruptedException e) {}
-	}
-	race.winner.compareAndSet(-1, number);
+    int minSleep = race.getMinSleep();
+    int maxSleep = race.getMaxSleep();
+    int distance = race.getDistance();
+    Random random = new Random();
+    for (int i = 0; i < distance; i++) {
+        try {
+            sleep(random.nextInt(minSleep, maxSleep + 1));
+            System.out.printf("%d - step %d\n",number, i);
+        } catch (InterruptedException e) {}
+    }
+    synchronized(race) {
+            race.addRacer(number);
+        }
 }
+
 }
